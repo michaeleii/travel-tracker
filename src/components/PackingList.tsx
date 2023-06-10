5190ea2334b6ea2b633ccc544d55e57b44fb8cfc
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-06-09 22:27:01 -0700

    Add sort

--- src/components/PackingList.tsx
+++ src/components/PackingList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IItem } from "../App";
 import Item from "./Item";
 import { motion, AnimatePresence } from "framer-motion";
@@ -11,11 +12,26 @@ function PackingList({
   onRemoveFromPackingList: (id: number) => void;
   onPackItem: (id: number) => void;
 }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems: IItem[];
+
+  if (sortBy === "description") {
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  } else if (sortBy === "packed") {
+    sortedItems = [...items].sort(
+      (a, b) => Number(a.packed) - Number(b.packed)
+    );
+  } else {
+    sortedItems = [...items];
+  }
+
   return (
-    <div className="bg-accent p-5">
+    <div className="flex flex-col justify-between bg-accent p-5">
       <ul className="flex flex-wrap justify-center gap-5">
         <AnimatePresence>
-          {items.map((item) => (
+          {sortedItems.map((item) => (
             <motion.div
               key={item.id}
               initial={{ opacity: 0 }}
@@ -32,6 +48,20 @@ function PackingList({
           ))}
         </AnimatePresence>
       </ul>
+      <div className="flex justify-center gap-5">
+        <select
+          className="select text-xl"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button className="btn-primary btn" onClick={() => setSortBy("input")}>
+          Clear List
+        </button>
+      </div>
     </div>
   );
 }
